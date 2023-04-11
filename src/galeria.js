1e63d5fc071de2f98bfd332d7a7769252d4e0581
beginoffile <cursos.mapatupisaro@gmail.com>
2023-04-11 00:30:32 -0600

    initial commit

---
+++ src/galeria.js
@@ -0,0 +1,17 @@
+import './app.scss';
+
+// import imgUrl_1 from './assets/img/galeria/galeria_01';
+// import imgUrl_2 from './assets/img/galeria/galeria_02.jpg';
+// import imgUrl_3 from './assets/img/galeria/galeria_03.jpg';
+// import imgUrl_4 from './assets/img/galeria/galeria_04.jpg';
+// import imgUrl_5 from './assets/img/galeria/galeria_05.jpg';
+// import imgUrl_6 from './assets/img/galeria/galeria_06.jpg';
+// import imgUrl_7 from './assets/img/galeria/galeria_07.jpg';
+// import imgUrl_8 from './assets/img/galeria/galeria_08.jpg';
+// import imgUrl_9 from './assets/img/galeria/galeria_09.jpg';
+// import imgUrl_10 from './assets/img/galeria/galeria_10.jpg';
+
+// console.log(imgUrl_1,imgUrl_2);
+// document.querySelectorAll('.imagen').forEach((imagen, i)=>{
+//     imagen.src = `imgUrl_1${i}`;
+// })
---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite';
+// import {resolve} from 'path';
+
+
+// import reactRefresh from '@vitejs/plugin-react-refresh';
+
+// import { createHtmlPlugin } from 'vite-plugin-html';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // plugins: [
+  //   createHtmlPlugin({
+  //     minify: false,
+  //     pages: [
+  //       {
+  //         entry: './main.js',
+  //         filename: 'index.html',
+  //         template: './src/index.html',  
+  //       }
+  //     ],
+  //   }),
+  // ],
+  // appType:'mpa',
+  root: 'src',
+  // assetsInclude:['src/assets/img/*.jpg'],
+  plugins:[],
+  build:{
+    outDir: 'dist',
+    emptyOutDir:true,
+    rollupOptions:{
+      input:{
+        main: './src/index.html',
+        contacto: 'src/contacto.html',
+        galeria: 'src/galeria.html',
+        nosotros: 'src/nosotros.html',
+        menu: 'src/menu.html',
+        proceso:'src/proceso.html'
+      }
+    }
+  }
+});
